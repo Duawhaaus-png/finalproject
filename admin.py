86751f713b24a849e09b36752fbf1bbc55185480
Duawhaaus-png <kimcheese2012@gmail.com>
2026-07-30 11:46:02 +0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,30 +1,37 @@
 from django.contrib import admin
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
+
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 4
+
 
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 2
 
+
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ('text', 'lesson', 'grade')
 
+
 class LessonAdmin(admin.ModelAdmin):
     inlines = [QuestionInline]
+
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
+
 
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
+
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
